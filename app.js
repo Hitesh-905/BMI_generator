7bd4b85b6aac2565165d5397b0d8238695b06226
Hitesh-905 <choudharyhitesh905@gmail.com>
2025-09-10 15:48:02 +0000

    js logic and updated UI

---
+++ app.js
@@ -0,0 +1,23 @@
+const form=document.querySelector('form');
+console.log(form);
+
+form.addEventListener('submit',function (e){
+    e.preventDefault();//to prevent directly geting the form submit
+
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const result = (document.querySelector('#results'))
+
+    if(height===" " || height<0 || isNaN(height)){
+        result.innerHTML=("Please Enter a valid Height")
+    }
+    else if (weight===" " || weight < 0 || isNaN(weight)){
+                result.innerHTML=("Please Enter a valid weight")
+    }
+    else{
+         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+         result.innerHTML=`<span>${bmi}</span>`;
+         
+    }
+})
